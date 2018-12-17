73cbcca3e2bc12d06490aa9830929f586be5ae37
rajput <manishkumarsingh2602@gmail.com>
2018-12-17 07:43:31 +0530

    7 th commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 def post_list(request):
